aa704678b1634a3bd2b56c926cb222c0c84ab55b
TonyAnaya <jaanaya99@gmail.com>
2022-02-26 12:07:57 -0600

    [ADD] Se crearon componentes Navbar, Servicios, Contactanos y Footer

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -4,19 +4,23 @@ import Navbar from '../components/Navbar/Navbar'
 import Servicios from '../components/Servicios/Servicios'
 import Contactanos from '../components/Contactanos/Contactanos'
 import Footer from '../components/Footer/Footer'
+import Conocenos from "../components/Conocenos/Conocenos";
 
 
 
 const Landing = () => {
   return (
-    <div style={{backgroundColor:'LightGreen'}}>
+    <div style={{backgroundColor:'DarkSeaGreen'}}>
     <Navbar/>
     <Carrousel/>
     <Servicios/>
+    <Conocenos />
     <Contactanos/>
     <Footer/>
     </div>
   )
 }
 
-export default Landing
+
+
+export default Landing;
